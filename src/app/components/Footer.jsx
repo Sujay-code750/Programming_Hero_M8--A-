2bf60bbf5917ca7ebeccbca6673626ba6d24ccb3
Sujoy <sujaykarmokar86@gmail.com>
2026-07-03 22:20:34 +0600

    added navbar and footer section.

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,9 +1,71 @@
-import React from 'react';
+import Image from "next/image";
+import logoImg from "../../../public/images/logo.png";
+import Link from "next/link";
+import { FaFacebookF } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { BsInstagram } from "react-icons/bs";
+import { SlSocialPintarest } from "react-icons/sl";
 
 const Footer = () => {
     return (
-        <div>
-            
+        <div className="bg-slate-900 text-white">
+            <div className="container mx-auto">
+                <footer className="footer sm:footer-horizontal p-10">
+  <nav>
+    <div>
+        <a className="flex items-center text-2xl font-bold text-white">
+                      <span>
+                        {" "}
+                        <Image src={logoImg} width={40} height={40} alt="Sunchart" />
+                      </span>{" "}
+                      SunChart
+                    </a>
+                    <p className="text-[#9fa2aa] py-5">You one-step shop for premium <br /> summer essentials</p>
+                    <ul className="flex gap-5 py-2">
+                        <li><a href="#" className="text-xl"><FaFacebookF /></a></li>
+                        <li><a href="#" className="text-xl"><FaXTwitter /></a></li>
+                        <li><a href="#" className="text-xl"><BsInstagram /></a></li>
+                        <li><a href="#" className="text-xl"><SlSocialPintarest /></a></li>
+                    </ul>
+    </div>
+  </nav>
+  <nav>
+    <h6 className="footer-title">Shop</h6>
+    <a className="link link-hover">Branding</a>
+    <a className="link link-hover">Design</a>
+    <a className="link link-hover">Marketing</a>
+    <a className="link link-hover">Advertisement</a>
+  </nav>
+  <nav>
+    <h6 className="footer-title">Company</h6>
+    <a className="link link-hover">About us</a>
+    <a className="link link-hover">Contact</a>
+    <a className="link link-hover">Jobs</a>
+    <a className="link link-hover">Press kit</a>
+  </nav>
+  <nav>
+    <h6 className="footer-title">Support</h6>
+    <a className="link link-hover">Terms of use</a>
+    <a className="link link-hover">Privacy policy</a>
+    <a className="link link-hover">Cookie policy</a>
+  </nav>
+  <form>
+    <h6 className="footer-title">Subscribe to our newsletter</h6>
+    <fieldset className="">
+      <div className="join">
+        <input
+          type="text"
+          placeholder="Enter your email address"
+          className="input text-black" />
+        <button className="btn bg-amber-500 join-item text-white">Subscribe</button>
+      </div>
+    </fieldset>
+  </form>
+</footer>
+ <div className="text-center text-white pb-5">
+    <p>@2026 SunCart. All rights reserved.</p>
+  </div>
+            </div>
         </div>
     );
 };
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,10 +1,88 @@
+import Image from "next/image";
+import logoImg from "../../../public/images/logo.png";
+import Link from "next/link";
 
 const Navbar = () => {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <div className="container mx-auto">
+        <div className="navbar">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  {" "}
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />{" "}
+                </svg>
+              </div>
+              <ul
+                tabIndex="-1"
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+              >
+                <li>
+                  <a>Item 1</a>
+                </li>
+                <li>
+                  <a>Parent</a>
+                  <ul className="p-2">
+                    <li>
+                      <a>Submenu 1</a>
+                    </li>
+                    <li>
+                      <a>Submenu 2</a>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a>Item 3</a>
+                </li>
+              </ul>
+            </div>
+            <a className="flex items-center font-bold text-xl bg-transparent border-none">
+              <span>
+                {" "}
+                <Image src={logoImg} width={40} height={40} alt="Sunchart" />
+              </span>{" "}
+              SunChart
+            </a>
+          </div>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">
+              <li>
+                <Link href={"/"} className="font-semibold text-lg">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link href={"/products"} className="font-semibold text-lg">
+                  Products
+                </Link>
+              </li>
+              <li>
+                <Link href={"/my-profile"} className="font-semibold text-lg">
+                  My Profile
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="navbar-end">
+            <button className="btn text-white bg-amber-500">Login / Register</button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
