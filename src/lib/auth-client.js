e839a80d8e5b202cd84c516d93eb5b316b420b70
Sujoy <sujaykarmokar86@gmail.com>
2026-07-05 02:30:55 +0600

    added some changes

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -3,5 +3,5 @@ export const { signIn, signUp, useSession } = createAuthClient()
 
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
+    baseURL: "https://programming-hero-m8-a.vercel.app"
 })
