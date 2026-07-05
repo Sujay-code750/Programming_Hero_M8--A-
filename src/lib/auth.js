d44afd5f1e00a325176269081a42f61e9da54d16
Sujoy <sujaykarmokar86@gmail.com>
2026-07-05 19:46:29 +0600

    add trusted origins

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,7 +2,7 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient(process.env.MONGOBD_URI);
+const client = new MongoClient(process.env.MONGODB_URI);
 const db = client.db("sunchart");
 
 export const auth = betterAuth({
